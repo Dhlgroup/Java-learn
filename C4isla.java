f9febf3f01c0f887cd74cf0a451446071665a1f8
Dhlgroup <shu.saura25@gmail.com>
2025-08-26 19:13:04 +0300

    Vers2

--- C4isla.java
+++ C4isla.java
@@ -5,11 +5,28 @@ public class C4isla {
         int  lastDinozavrWas = 1_256_653_145;
         long myMoney = 4_000_657_896L;
         String tale = "nine";
-        
+
+
         System.out.println("I have "+ tale+" Tales");
         System.out.println("MyMoney = "+ myMoney);
         System.out.println("LastDinozavrWas = "+ lastDinozavrWas);
         System.out.println("year = "+year);
         System.out.println("Age = "+age);
+
+
+        float cornerDigre = 43.2F;
+        double temprecha1 = 37.9D;
+        double workProcentReady = 78.7;
+
+        System.out.println("Corner Digre = "+ cornerDigre);
+        System.out.println("Temprecha 1 = "+ temprecha1);
+        System.out.println("Work Ready = " + workProcentReady);
+
+        double temprecha2 = -16.7;
+        double togetherTemp = temprecha1 + temprecha2;
+
+        System.out.println("Together temp = " + temprecha1 + temprecha2);
+        System.out.println("Together = "+ togetherTemp);
+        
     }
 }
