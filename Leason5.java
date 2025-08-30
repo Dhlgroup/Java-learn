8df3030aa6c51000a321e07555869e45f8b79631
Dhlgroup <shu.saura25@gmail.com>
2025-08-30 20:51:17 +0300

    Version 7

--- Leason5.java
+++ Leason5.java
@@ -1,6 +1,6 @@
 public class Leason5 {
     public static void main(String[] args) {
-      // Example 1
+      // Example 1 Expressions
         
         double distance = 250.9;
         double time = 3.4;
@@ -8,7 +8,7 @@ public class Leason5 {
 
         System.out.println("Speed = "+ speed);
 
-        // Example 2
+        // Example 2 Conditional Expressions
         // First Version:
 
         int lemons = 8;
@@ -27,8 +27,24 @@ public class Leason5 {
         //     System.out.println("You have a few fruits");
         // }
 
-        // Example 3
+        // Example 3 Declaration Statement
+
+        int initialBalance = 700;
+        initialBalance -= 90; // Expression Statemant
+
+        System.out.println("New Balance = " + initialBalance);
 
 
+        // Example 4 Conditional Statement
+
+        int score =0;
+        score++;
+
+        if (score > 0) {
+            System.out.println("You earn " + score +" point");
+        } 
+        else { System.out.println("You have Zero point");
+    }
+    
     }
 }
